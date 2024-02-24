c5781c5758eccb731f8fa9a49309507f6740d34e
IsmailBENJAAFAR <benjaafarismail20@gmail.com>
2024-02-24 18:59:01 +0100

    initial commit

---
+++ src/main/java/com/example/minishop_backend/MinishopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.minishop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinishopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinishopBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/minishop_backend/MinishopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minishop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinishopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
