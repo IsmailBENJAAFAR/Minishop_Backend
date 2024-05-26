39862a9502d1544afe7e6fe51cbe2e7b96d457bf
IsmailBENJAAFAR <benjaafarismail20@gmail.com>
2024-05-26 13:05:02 +0100

    add crud operations for  commande

--- src/main/java/com/example/minishop_backend/commande/CommandeService.java
+++ src/main/java/com/example/minishop_backend/commande/CommandeService.java
@@ -1,24 +1,39 @@
 package com.example.minishop_backend.commande;
 
+import com.example.minishop_backend.notation.Notation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @Service
+@RequiredArgsConstructor
 public class CommandeService {
+    private final CommandeRepository commandeRepository;
     public List<Commande> getCommandes(){
-        return null;
+        return commandeRepository.findAll();
     }
     public void addCommande(Commande commande){
-
+        commandeRepository.save(commande);
     }
-    public void updateCommande(Long id){
-
+    @Transactional
+    public void updateCommande(Long id, Date date){
+        Commande oldCommande = commandeRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException(
+                        "Commande with id " + id + " does not exists"));;
+        if (date!=null &&
+                !Objects.equals(oldCommande.getDate(),date)){
+            oldCommande.setDate(date);
+        }
     }
-    public void deleteCommande(){
-
-    }
-    public List<Commande> searchCommande(){
-        return null;
+    public void deleteCommande(Long id){
+        boolean exists = commandeRepository.existsById(id);
+        if (!exists){
+            throw new IllegalStateException("Commande with id " + id + " does not exists");
+        }
+        commandeRepository.deleteById(id);
     }
 }
