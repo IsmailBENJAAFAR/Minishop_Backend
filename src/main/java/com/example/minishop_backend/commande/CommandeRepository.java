40d17faf28e10e84c0cbc87b4e1593e686dc2410
IsmailBENJAAFAR <benjaafarismail20@gmail.com>
2024-05-26 12:55:25 +0100

    add repository for commande

---
+++ src/main/java/com/example/minishop_backend/commande/CommandeRepository.java
@@ -0,0 +1,8 @@
+package com.example.minishop_backend.commande;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommandeRepository extends JpaRepository<Commande,Long> {
+}
