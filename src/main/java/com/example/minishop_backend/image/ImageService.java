7b517d690dc1b043cd5f8c12f6b28c3755ad44a8
IsmailBENJAAFAR <benjaafarismail20@gmail.com>
2024-05-26 11:52:54 +0100

    add crud operations for Image class

--- src/main/java/com/example/minishop_backend/image/ImageService.java
+++ src/main/java/com/example/minishop_backend/image/ImageService.java
@@ -1,24 +1,38 @@
 package com.example.minishop_backend.image;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
+@RequiredArgsConstructor
 public class ImageService {
+    private final ImageRepository imageRepository;
     public List<Image> getImages(){
-        return null;
+        return imageRepository.findAll();
     }
     public void addImage(Image image){
-
+        imageRepository.save(image);
     }
-    public void updateImage(Long id){
-
+    @Transactional
+    public void updateImage(Long id,String url){
+        Image oldImage = imageRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException(
+                        "Image with id " + id + " does not exists"));
+        if(url != null &&
+                !url.isEmpty()&&
+                !Objects.equals(oldImage.getUrl(),url)){
+            oldImage.setUrl(url);
+        }
     }
-    public void deleteImage(){
-
-    }
-    public List<Image> searchImage(){
-        return null;
+    public void deleteImage(Long id){
+        boolean exists = imageRepository.existsById(id);
+        if (!exists){
+            throw new IllegalStateException("image with id " + id + " does not exists");
+        }
+        imageRepository.deleteById(id);
     }
 }
