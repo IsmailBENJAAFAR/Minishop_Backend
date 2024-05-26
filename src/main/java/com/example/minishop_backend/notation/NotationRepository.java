55bd98d7fe437c6eeba8bd7b7f4314b2c471f90a
IsmailBENJAAFAR <benjaafarismail20@gmail.com>
2024-05-26 12:07:27 +0100

    add repository interface for notation

---
+++ src/main/java/com/example/minishop_backend/notation/NotationRepository.java
@@ -0,0 +1,6 @@
+package com.example.minishop_backend.notation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotationRepository extends JpaRepository<Notation,Long> {
+}
