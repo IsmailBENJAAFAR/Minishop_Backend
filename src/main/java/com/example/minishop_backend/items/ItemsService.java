a02a736987045c6fba317edfcfca13ec5bd1bb5f
IsmailBENJAAFAR <benjaafarismail20@gmail.com>
2024-05-26 12:46:58 +0100

    add crud operations for items

--- src/main/java/com/example/minishop_backend/items/ItemsService.java
+++ src/main/java/com/example/minishop_backend/items/ItemsService.java
@@ -1,22 +1,37 @@
 package com.example.minishop_backend.items;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ItemsService {
+    private final ItemsRepository itemsRepository;
     public List<Items> getItems(){
-        return null;
+        return itemsRepository.findAll();
     }
     public void addItems(Items items){
-
+        itemsRepository.save(items);
     }
-    public void updateItems(Long id){
-
+    @Transactional
+    public void updateItems(Long id,int quantity){
+        Items oldItem = itemsRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException(
+                        "Item with id " + id + " does not exists"));;
+        if (quantity>0 &&
+                oldItem.getQuantity() != quantity){
+            oldItem.setQuantity(quantity);
+        }
     }
-    public void deleteItems(){
-
+    public void deleteItems(Long id){
+        boolean exists = itemsRepository.existsById(id);
+        if (!exists){
+            throw new IllegalStateException("Item with id " + id + " does not exists");
+        }
+        itemsRepository.deleteById(id);
     }
     public List<Items> searchItems(){
         return null;
