a04da5e58e5fd92f8cc7b21025e80ee8f93f9aff
IsmailBENJAAFAR <benjaafarismail20@gmail.com>
2024-05-26 12:14:54 +0100

    add crud operations for notation

--- src/main/java/com/example/minishop_backend/notation/NotationRepository.java
+++ src/main/java/com/example/minishop_backend/notation/NotationRepository.java
@@ -1,6 +1,8 @@
 package com.example.minishop_backend.notation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NotationRepository extends JpaRepository<Notation,Long> {
 }
--- src/main/java/com/example/minishop_backend/notation/NotationService.java
+++ src/main/java/com/example/minishop_backend/notation/NotationService.java
@@ -1,24 +1,37 @@
 package com.example.minishop_backend.notation;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class NotationService {
+    private final NotationRepository notationRepository;
     public List<Notation> getNotations(){
-        return null;
+        return notationRepository.findAll();
     }
     public void addNotation(Notation notation){
-
+        notationRepository.save(notation);
     }
-    public void updateNotation(Long id){
-
+    @Transactional
+    public void updateNotation(Long id,int etoiles){
+        Notation oldNotation = notationRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException(
+                        "Notation with id " + id + " does not exists"));;
+        if (etoiles>0 &&
+                oldNotation.getEtoiles() != etoiles){
+            oldNotation.setEtoiles(etoiles);
+        }
     }
-    public void deleteNotation(){
-
-    }
-    public List<Notation> searchNotation(){
-        return null;
+    public void deleteNotation(Long id){
+        boolean exists = notationRepository.existsById(id);
+        if (!exists){
+            throw new IllegalStateException("Notation with id " + id + " does not exists");
+        }
+        notationRepository.deleteById(id);
     }
 }
