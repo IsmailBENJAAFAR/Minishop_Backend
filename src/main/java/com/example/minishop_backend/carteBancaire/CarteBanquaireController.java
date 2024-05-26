c409ffd3cf6461ff62e271599c0070415aec5f62
IsmailBENJAAFAR <benjaafarismail20@gmail.com>
2024-05-26 14:22:12 +0100

    add repository for carte bancaire

--- src/main/java/com/example/minishop_backend/carteBancaire/CarteBanquaireController.java
+++ src/main/java/com/example/minishop_backend/carteBancaire/CarteBanquaireController.java
@@ -1,10 +1,12 @@
 package com.example.minishop_backend.carteBancaire;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/")
+@CrossOrigin(origins = "http://localhost:5173")
+@RequestMapping(path = "cartebancaire")
 public class CarteBanquaireController {
 
 }
