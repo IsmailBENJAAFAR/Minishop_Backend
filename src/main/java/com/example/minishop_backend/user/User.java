d09e6fa2e5d794695615d9015278cc29301b4fe2
samini <soufianeamini2002@gmail.com>
2024-05-29 22:29:33 +0100

    added getter for favourites

--- src/main/java/com/example/minishop_backend/user/User.java
+++ src/main/java/com/example/minishop_backend/user/User.java
@@ -46,6 +46,10 @@ public class User implements UserDetails {
 
     }
 
+    public List<Produit> getProduits() {
+        return produits;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(role.name()));
